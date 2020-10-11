b54c9dae54b11df274d361e62045e4339d42ae8c
sami-chaaban <36083637+sami-chaaban@users.noreply.github.com>
2020-10-11 19:52:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,138 @@
+# StarParser
+
+Use this script to manipulate Relion 3.1 star files.
+
+Usage:
+
+```
+python3 starparser.py --i input.star [options]
+```
+
+## Getting Started
+
+* You need to have Python 3 installed and have the **pandas** and **optparse** packages.
+
+* Your file needs to be a standard Relion 3.1 star file with an optics table, particle table, and particle list with tab delimited columns.
+
+## Options
+
+* ```--plot_defocus``` Plot defocus to Defocus_histogram.png. Can be used with -c and -q for a subset count, otherwise plots all.
+
+* ```--delete_column``` Delete column and renumber headers. E.g. \_rlnMicrographName. To enter multiple columns, separate them with a slash: \_rlnMicrographName/\_rlnCoordinateX.
+
+* ```--delete_particles``` Delete particles. Pick a column header (-c) and query (-q) to delete particles that match it.
+
+* ```--extract_particles``` Write a star file with particles that match a column header (-c) and query (-q).
+
+* ```--count_particles``` Count particles and print the result. Can be used with -c and -q for a subset count, otherwise counts all.
+
+* ```--count_mics``` Count the number of unique micrographs. Can be used with -c and -q for a subset count, otherwise counts all.
+
+* ```--max_defocus``` Extract particles with defocus values less than this value (Angstroms). Can be used with -c and -q to only consider a subset.
+
+* ```--list_column``` Write all values of a column to a file (filename is the header). E.g. \_rlnMicrographName. To enter multiple columns, separate them with a slash: \_rlnMicrographName/\_rlnCoordinateX. Can be used with -c and -q for a subset count, otherwise lists all items.
+
+* ```--swap_columns``` Swap columns from another star file (specified with -f). E.g. \_rlnMicrographName. To enter multiple columns, separate them with a slash: \_rlnMicrographName/\_rlnCoordinateX.
+
+* ```--relegate``` Remove optics table and optics column. This may not be sufficient to be fully compatible with Relion 3.0. Use --delete_column to remove other bad columns before this, if necessary.
+
+* ```-f``` Name of second file to extract columns from.
+
+* ```-c``` Column query. E.g. \_rlnMicrographName. To enter multiple columns, separate them with a slash: \_rlnMicrographName/\_rlnCoordinateX.
+
+* ```-q``` Particle query. To enter multiple columns, separate them with a slash: 20200101/20200203.
+
+* ```-o``` Output file name. Default is output.star
+
+## Examples
+
+* Plot a histogram of defocus values.
+
+```
+python3 starparser.py --i run_data.star --plot_defocus
+```
+
+* Plot a histogram of defocus values from a subset of micrographs that contain "200826" or "200827".
+
+```
+python3 starparser.py --i run_data.star --plot_defocus -c _rlnMicrographName -q 200826/200827
+```
+
+* Delete the \_rlnCtfMaxResolution and \_rlnCtfFigureOfMerit columns.
+
+```
+python3 starparser.py --i run_data.star --delete_column _rlnCtfMaxResolution/_rlnCtfFigureOfMerit -o output.star
+```
+
+* Delete all particles with "200702" or "200715" in the \_rlnMicrographName column.
+
+```
+python3 starparser.py --i run_data.star --delete_particles -c _rlnMicrographName -q 200702/200715 -o output.star
+```
+
+* Make a new star file with only particles that have "1" in the \_rlnClassNumber column.
+
+```
+python3 starparser.py --i run_data.star --extract_particles -c _rlnClassNumber -q 1 -o output.star
+```
+
+* Count the number of particles with "200702" or "200715" in the \_rlnMicrographName column.
+
+```
+python3 starparser.py --i run_data.star --count_particles -c _rlnMicrographName -q 200702/200715 -o output.star
+```
+
+* Count the total number of unique micrographs.
+
+```
+python3 starparser.py --i run_data.star --count_mics
+```
+
+* Count the total number of unique micrographs in the subset that contain "200826" in the \_rlnMicrographName column.
+
+```
+python3 starparser.py --i run_data.star --count_mics -c _rlnMicrographName -q 200826
+```
+* Extract particles with defocus values (in \_rlnDefocusU) less than this value and write to a new file.
+
+```
+python3 starparser.py --i run_data.star --max_defocus 40000 -o output.star
+```
+* Extract particles with defocus values (in \_rlnDefocusU) less than this value that also have "200826" in the \_rlnMicrographName column, and write to a new file.
+
+```
+python3 starparser.py --i run_data.star --max_defocus 40000 -c _rlnMicrographName -q 200826 -o output.star
+```
+* List all micrographs in the star file to a new text file.
+
+```
+python3 starparser.py --i run_data.star --list_column _rlnMicrographName
+```
+
+* List all defocus values and x-coordinate values in the star file to new text files.
+
+```
+python3 starparser.py --i run_data.star --list_column _rlnDefocusU/_rlnCoordinateX
+```
+
+* List all defocus values of particles that contain "200826" in the \_rlnMicrographName column.
+
+```
+python3 starparser.py --i run_data.star --list_column _rlnDefocusU -c _rlnMicrographName -q 200826
+```
+
+* Swap the \_rlnAnglePsi, \_rlnAngleRot, \_rlnAngleTilt, \_rlnNormCorrection, \_rlnLogLikeliContribution, \_rlnMaxValueProbDistribution, \_rlnNrOfSignificantSamples, \_rlnOriginXAngst, \_rlnOriginYAngst from file2.star into run_data.star
+
+```
+python3 starparser.py --i run_data.star --swap_columns _rlnAnglePsi/_rlnAngleRot/_rlnAngleTilt/_rlnNormCorrection/_rlnLogLikeliContribution/_rlnMaxValueProbDistribution/_rlnNrOfSignificantSamples/_rlnOriginXAngst/_rlnOriginYAngst -f file2.star -o output.star
+```
+
+* Relegate the star file to on compatible with Relion 3.0.
+
+```
+python3 starparser.py --i run_data.star --relegate -o output.star
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ starparser.py
@@ -0,0 +1,484 @@
+import sys
+import pandas as pd
+import optparse
+
+def setupParserOptions():
+    
+    parser = optparse.OptionParser(usage="Usage: %prog [options]",
+        version="%prog 1.0.")
+
+    parser.add_option("--i",
+        action="store", dest="file",
+        help="Input file name.")
+
+    parser.add_option("--plot_defocus",
+        action="store_true", dest="parser_plotdefocus", default=False,
+        help="Plot defocus to Defocus_histogram.png. Can be used with -c and -q for a subset count, otherwise plots all.")
+    
+    parser.add_option("--delete_column",
+        action="store", dest="parser_delcolumn", type="string", default="",
+        help="Delete column and renumber headers. E.g. _rlnMicrographName. To enter multiple columns, separate them with a slash: _rlnMicrographName/_rlnCoordinateX.")
+    
+    parser.add_option("--delete_particles",
+        action="store_true", dest="parser_delparticles", default=False,
+        help="Delete particles. Pick a column header (-c) and query (-q) to delete particles that match it.")
+    
+    parser.add_option("--extract_particles",
+        action="store_true", dest="parser_extractparticles", default=False,
+        help="Write a star file with particles that match a column header (-c) and query (-q).")
+
+    parser.add_option("--count_particles",
+        action="store_true", dest="parser_countme", default=False,
+        help="Count particles. Can be used with -c and -q for a subset count, otherwise counts all.")
+    
+    parser.add_option("--count_mics",
+        action="store_true", dest="parser_uniquemics", default=False,
+        help="Count the number of unique micrographs. Can be used with -c and -q for a subset count, otherwise counts all.")
+    
+    parser.add_option("--max_defocus",
+        action="store", dest="parser_maxdefocus", type="float", default = 0,
+        help="Maximum defocus.")
+    
+    parser.add_option("--list_column",
+        action="store", dest="parser_writecol", type="string", default="",
+        help="Write all values of a column to a file (filename is the header). E.g. _rlnMicrographName. To enter multiple columns, separate them with a slash: _rlnMicrographName/_rlnCoordinateX. Can be used with -c and -q for a subset count, otherwise lists all items.")
+    
+    parser.add_option("-c",
+        action="store", dest="parser_column", type="string", default="",
+        help="Column query. E.g. _rlnMicrographName. To enter multiple columns, separate them with a slash: _rlnMicrographName/_rlnCoordinateX.")
+    
+    parser.add_option("-q",
+        action="store", dest="parser_query", type="string", default="",
+        help="String query. To enter multiple columns, separate them with a slash: 20200101/20200203.")
+    
+    parser.add_option("--swap_columns",
+        action="store", dest="parser_swapcolumns", type="string", default="",
+        help="Swap columns from another star file (specified with -f). E.g. _rlnMicrographName. To enter multiple columns, separate them with a slash: _rlnMicrographName/_rlnCoordinateX.")
+    
+    parser.add_option("-f",
+        action="store", dest="parser_file2", type="string", default="",
+        help="Name of second file to extract columns from. Pick column headers with -c.")
+    
+    parser.add_option("--relegate",
+        action="store_true", dest="parser_relegate", default=False,
+        help="Remove optics table and Optics column, renumber headers. This may not be sufficient to be fully compatible with Relion 3.0. Use --delete_column to remove other bad columns before this, if necessary.")
+    
+    parser.add_option("-o",
+        action="store", dest="parser_output", type="string", default = "output.star",
+        help="Output file name.")
+
+    options,args = parser.parse_args()
+
+    if len(sys.argv) < 4:
+            parser.print_help()
+            sys.exit()
+
+    params={}
+
+    for i in parser.option_list:
+            if isinstance(i.dest,str):
+                    params[i.dest] = getattr(options,i.dest)
+                    
+    return(params)
+
+def parsestar(starfile):
+
+    starfilesplit = starfile.split()
+
+    version = starfilesplit[0:3]
+
+    opticsstop = 0
+
+    for i in range(4,100):
+        
+        if starfilesplit[i] == '#':
+            
+            opticsstop = i
+            
+            break
+            
+    if opticsstop == 0:
+        
+        print('Could not find end of optics table. Exiting.')
+        
+        sys.exit()
+
+    particlesstop = 0
+            
+    for i in range(opticsstop+3,200):
+
+        if starfilesplit[i].replace('.','',1).replace('-','',1).isnumeric() or ".mrc" in starfilesplit[i]:
+            
+            particlesstop = i
+            
+            break
+
+    if particlesstop == 0:
+
+        print('Could not find end of particles table. Exiting.')
+
+        sys.exit()
+
+    opticstable = starfilesplit[3:opticsstop]
+
+    particlestable = starfilesplit[opticsstop+3:particlesstop]
+    
+    headers = []
+    for m in particlestable[::2][1:]: 
+        headers.append(m)
+
+    particles = starfilesplit[particlesstop:]
+
+    return(version,opticstable,headers,particles)
+
+#######################################################################################################
+
+def getparticles(filename):
+    
+    file = open(filename,mode='r')
+    starfile = file.read()
+    file.close()
+
+    version, opticstable, headers, particles = parsestar(starfile)
+    
+    metadata = [version,opticstable,headers]
+
+    totalcolumns = len(headers)
+
+    particles_lst = [particles[x:x+totalcolumns] for x in range(0, len(particles), totalcolumns)]
+
+    allparticles = pd.DataFrame(particles_lst, columns = headers)
+
+    return(allparticles, metadata)
+
+def countparticles(particles):
+
+    totalparticles = len(particles.index)
+    print('\nThere are ' + str(totalparticles) + ' particles in total.\n')    
+
+def countqueryparticles(particles,columns,query):
+
+    totalparticles = len(particles.index)
+    
+    totalquery = 0
+    
+    if len(columns)>1:
+        print("\Error: you have specified two different columns.\n")
+        sys.exit()
+    
+    q = "|".join(query)
+    totalquery += len(particles[particles[columns[0]].str.contains(q)].index)
+        
+    percentparticles = round(totalquery*100/totalparticles,1)
+
+    print('\nThere are ' + str(totalquery) + ' particles that match ' + query + ' in the specified columns (out of ' + str(totalparticles) + ', or ' + str(percentparticles) + '%).\n')
+
+def plotdefocus(particles):
+    
+    particles["_rlnDefocusU"] = pd.to_numeric(particles["_rlnDefocusU"], downcast="float")
+
+    ax = particles["_rlnDefocusU"].plot.hist(bins=400)
+    ax.set_xlabel("DefocusU")
+    
+    fig = ax.get_figure()
+    fig.savefig('Defocus_histogram.png')
+    
+def maxdefocus(particles, maxvalue):
+    
+    particles["_rlnDefocusU_float"] = particles["_rlnDefocusU"]
+    particles["_rlnDefocusU_float"] = pd.to_numeric(particles["_rlnDefocusU_float"], downcast="float")
+    purgedparticles = particles.copy()
+    purgedparticles = purgedparticles[purgedparticles["_rlnDefocusU_float"]<maxvalue]
+    
+    purgednumber = len(particles.index) - len(purgedparticles.index)
+    
+    particles.drop("_rlnDefocusU_float",1, inplace=True)
+    purgedparticles.drop("_rlnDefocusU_float",1, inplace=True)
+    
+    return(purgedparticles, purgednumber)
+
+def delcolumn(particles, columns, metadata):
+    
+    nocolparticles = particles.copy()
+    
+    for c in columns:
+        nocolparticles.drop(c, 1, inplace=True)
+        metadata[2].remove(c)
+    
+    return(nocolparticles, metadata)
+
+def delparticles(particles, columns, query):
+    
+    purgedparticles = particles.copy()
+    
+    if len(columns)>1:
+        print("Error: you have specified two columns. You can't if you're querying to delete.\n")
+        sys.exit()
+        
+    q = "|".join(query)
+    
+    purgedparticles.drop(purgedparticles[purgedparticles[columns[0]].str.contains(q)].index , 0,inplace=True)
+    
+    return(purgedparticles)
+
+def extractparticles(particles, columns, query):
+
+    extractedparticles = particles.copy()
+    
+    if len(columns)>1:
+        print("Error: you have specified two columns. Only specify one if you're extracting from a subset of the data using a query.\n")
+        sys.exit()
+    
+    q = "|".join(query)
+    
+    extractedparticles.drop(extractedparticles[~extractedparticles[columns[0]].str.contains(q)].index, 0,inplace=True)
+    
+    extractednumber = len(extractedparticles.index)
+    
+    return(extractedparticles, extractednumber)
+
+def swapcolumns(original_particles, swapfrom_particles, columns):
+    
+    swappedparticles = original_particles.copy()
+    
+    for c in columns:
+        columnindex = original_particles.columns.get_loc(c)
+        swappedparticles.drop(c,1, inplace=True)
+        swappedparticles.insert(columnindex, c, swapfrom_particles[c].values.tolist())
+    
+    return(swappedparticles)
+
+def renumbercol(datatable, columns):
+    
+    newdatatable = datatable[:3]
+
+    newcount = 1
+
+    for i,L in enumerate(datatable[2:]):
+
+        if L in columns:
+
+            if i != 0:
+                newdatatable = newdatatable[:-1]
+                newcount -= 1
+            else:    
+                newdatatable = newdatatable[:-1]
+            continue
+
+        if L[0] == '#' and i != 1:
+            newdatatable.append('#' + str(newcount))
+            newcount += 1
+            continue
+
+        if i != 1:
+            newdatatable.append(L)
+            
+    return(newdatatable)
+
+def writestar(particles, metadata, outputname, relegate):
+    
+    output = open(outputname,"w")
+    
+    output.write('\n')
+
+    version = metadata[0]
+
+    for t in version:
+        output.write(t)
+        output.write(' ')
+    output.write('\n\n')
+    
+    if not relegate:
+
+        output.write('data_optics\n\n')
+        output.write('loop_\n')
+
+        opticstable = metadata[1][1:]
+
+        for n,i in enumerate(opticstable[1:]):
+
+            output.write(i)
+            output.write('\t')
+            if i[0] != '#':
+                if i[0] != '_':
+                    opticsvaluesstart=n+2
+                    break
+            else:
+                output.write("\n")
+
+        for i in opticstable[opticsvaluesstart:]:
+            output.write(i)
+            output.write('\t')
+        output.write('\n\n\n')
+
+        for t in version:
+            output.write(t)
+            output.write(' ')
+        output.write('\n\n')
+
+    output.write('data_particles\n\n')
+    output.write('loop_')
+
+    headers = metadata[2]
+    count=1
+    for p in headers:
+        output.write('\n')
+        output.write(p)
+        output.write(" #"+str(count))
+        count += 1
+
+    output.write('\n')
+    particles.to_csv(output, header=None, index=None, sep=' ', mode='a')
+
+    output.close()
+    
+def checksubset(particles, params):
+    
+    if params["parser_column"] != "" and params["parser_query"] != "":
+        query = params["parser_query"].split("/")
+        columns = params["parser_column"].split("/")
+        subsetparticles, extractednumber = extractparticles(particles, columns, query)
+        
+        print("\nCreating a subset of " + str(extractednumber) + " particles that match " + str(query) +               " in the columns " + str(columns) + " (or " + str(round(extractednumber*100/len(particles.index),1)) + "%)")
+        
+        return(subsetparticles)
+    
+    else:
+        return(particles)
+    
+def writecol(particles, columns):
+    
+    outputs = []
+    
+    for c in columns:
+        
+        tosave = particles[c].tolist()
+        
+        outputname = c[4:] + ".txt"
+        outputs.append(outputname)
+        
+        output = open(outputname,"w")
+        
+        for s in tosave:
+            output.write(s)
+            output.write("\n")
+        
+        output.close()
+    
+    return(outputs)
+
+########################################################
+    
+def mainloop(params):
+    
+    #CHECKS
+    
+    if 'file' not in params:
+        print("Error: no filename entered. See documentation with starparse.py -h.")
+        sys.exit()
+        
+    if "_rlnOpticsGroup" in params["parser_column"] and params["parser_relegate"]:
+        print("Error: cannot have the relegate option and the delete OpticsGroup column at the same time (the former will do the latter already).")
+        sys.exit()
+        
+        
+    ##################################
+    
+    filename = params['file']
+    
+    allparticles, metadata = getparticles(filename)
+
+    totalparticles = len(allparticles.index)
+    
+    if params["parser_query"] != "":
+        query = params["parser_query"].split("/")
+    
+    if params["parser_column"] != "":
+        columns = params["parser_column"].split("/")
+        
+        for c in columns:
+            if c not in allparticles.columns:
+                print("Error: " + str(c) + " does not exist in your star file.")
+                sys.exit()
+                
+    else:
+        
+        columns = ""
+        
+    relegateflag = params["parser_relegate"]
+        
+    #Set up jobs that probably don't require a subset (faster this way)
+    
+    if params["parser_countme"] and params["parser_column"] != "" and params["parser_query"] != "":
+        countqueryparticles(allparticles, columns, query)
+        sys.exit()
+    elif params["parser_countme"]:
+        countparticles(allparticles)
+        sys.exit()
+        
+    if params["parser_delcolumn"] != "":
+        columns = params["parser_delcolumn"].split("/")
+        newparticles, metadata = delcolumn(allparticles, columns, metadata)
+        writestar(newparticles, metadata, params["parser_output"], relegateflag)
+        print("\nRemoved the columns " + str(columns) + "\nOutput star file: " + params["parser_output"] + "\n")
+        sys.exit()
+        
+    if params["parser_delparticles"]:
+        newparticles = delparticles(allparticles, columns, query)
+        purgednumber = len(allparticles.index) - len(newparticles.index)
+        writestar(newparticles, metadata, params["parser_output"], relegateflag)
+        print("\nRemoved " + str(purgednumber) + " particles out of " + str(totalparticles) + " that had particles that matched " + str(query) +               " in the column " + params["parser_column"] + " (or " + str(round(purgednumber*100/totalparticles,1)) + "%).\n-->Output star file: " + params["parser_output"] + "\n")
+        sys.exit()
+        
+    if params["parser_swapcolumns"] != "":
+        otherparticles, metadata = getparticles(params["parser_file2"])
+        columstoswap = params["parser_swapcolumns"].split("/")
+        swappedparticles = swapcolumns(allparticles, otherparticles, columstoswap)
+        writestar(swappedparticles, metadata, params["parser_output"], relegateflag)
+        print("\nSwapped in " + str(columstoswap) + " from " + params["parser_file2"] +               "\n-->Output star file: " + params["parser_output"] + "\n")
+        sys.exit()
+    
+    ######
+    #setup SUBSET for remaining functions if necessary
+    
+    if relegateflag:
+        newparticles, metadata = delcolumn(allparticles, ["_rlnOpticsGroup"], metadata)
+        particles2use = checksubset(newparticles, params)
+    else:      
+        particles2use = checksubset(allparticles, params)
+    
+    if params["parser_uniquemics"]:
+        totalmics = len(particles2use["_rlnMicrographName"].unique())
+        print("\nThere are " + str(totalmics) + " unique micrographs in this dataset.\n")
+        sys.exit()
+
+    if params["parser_extractparticles"]:
+        writestar(particles2use, metadata, params["parser_output"], relegateflag)
+        print("\n-->Output star file: " + params["parser_output"] + "\n")
+        sys.exit()
+        
+    if params["parser_plotdefocus"]:
+        plotdefocus(particles2use)
+        print("\n-->Output to Defocus_histogram.png.\n")
+        sys.exit()
+        
+    if params["parser_maxdefocus"] != 0:
+        purgedparticles, purgednumber = maxdefocus(particles2use, params["parser_maxdefocus"])
+        writestar(purgedparticles, metadata, params["parser_output"], relegateflag)
+        print("\nRemoved " + str(purgednumber) + " particles out of " + str(len(purgedparticles.index)) + " that had defocus values above " + str(params["parser_maxdefocus"]) + " (or " +               str(round(purgednumber*100/totalparticles,1)) + "%).\n-->Output star file: " + params["parser_output"] + "\n")
+        sys.exit()
+        
+    if relegateflag:
+        writestar(particles2use, metadata, params["parser_output"], relegateflag)
+        print("\nRemoved the optics table and _rlnOpticsGroup.\n-->Output star file: " + params["parser_output"] + "\n")
+        sys.exit()
+        
+    if params["parser_writecol"] != "":
+        colstowrite = params["parser_writecol"].split("/")
+        outputs = writecol(particles2use, colstowrite)
+        print("\nWrote entries from " + str(colstowrite) + "\n-->Output files: " + str(outputs) + " \n")
+        sys.exit()
+
+if __name__ == "__main__":
+    params = setupParserOptions()
+    mainloop(params)
